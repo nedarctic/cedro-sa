28b837b8d0dc92d02b76d5237f03e4a0ea0755b4
nedarctic <nedarctic@gmail.com>
2026-06-10 09:03:59 +0300

    itineraries patch logic

--- app/api/itineraries/[tourId]/[itineraryId]/route.ts
+++ app/api/itineraries/[tourId]/[itineraryId]/route.ts
@@ -2,8 +2,8 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 import z from "zod";
 
-export async function PATCH(req: NextRequest, { params }: { params: Promise<{ tourId: string; itineraryId: string }> }) {
-    const { tourId, itineraryId } = await params;
+export async function PATCH(req: NextRequest, { params }: { params: Promise<{ itineraryId: string }> }) {
+    const { itineraryId } = await params;
 
     const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
     const refreshedToken = req.headers.get('x-refreshed-access-token');
@@ -28,7 +28,7 @@ export async function PATCH(req: NextRequest, { params }: { params: Promise<{ to
         dayImage: z
             .instanceof(File)
             .optional()
-            .refine(file => !file || file.size > 0, {message: 'file should be greater than 0MB'})
+            .refine(file => !file || file.size > 0, { message: 'file should be greater than 0MB' })
             .refine((file) =>
                 !file || ["image/png", "image/jpeg"].includes(file.type),
                 { message: "Only PNG and JPEG images are allowed" }
@@ -45,7 +45,7 @@ export async function PATCH(req: NextRequest, { params }: { params: Promise<{ to
         dayImage: formData.get('image')
     });
 
-    if(!parsedData.success){
+    if (!parsedData.success) {
         return NextResponse.json({
             success: false,
             error: parsedData.error.message || 'validation error'
@@ -53,6 +53,27 @@ export async function PATCH(req: NextRequest, { params }: { params: Promise<{ to
     }
 
     try {
+        const res = await fetch(`${process.env.BACKEND_API}/itineraries/${itineraryId}`, {
+            method: 'PATCH',
+            headers: {
+                Authorization: `Bearer ${token.access_token}`
+            },
+            body: formData,
+        });
 
-    } catch (error) {}
+        if (!res.ok) {
+            return NextResponse.json({
+                success: false, error: 'Backend request failed.'
+            }, { status: 400 })
+        }
+
+        const data = await res.json();
+
+        return NextResponse.json({ success: true, data })
+
+    } catch (error) {
+        return NextResponse.json({
+            success: false, error: error instanceof Error ? error.message : String(error)
+        }, { status: 500 })
+    }
 }
